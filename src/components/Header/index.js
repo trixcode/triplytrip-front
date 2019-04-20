aa1949741b81f2055e673d6f174866e9c6202afa
Randomir <chingizkg@gmail.com>
2019-04-20 20:27:28 +0600

    scss checked

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import './header.scss';
 const Header = (props) => {
     const {text} = props;
     return (
-        <div className="test">{text}</div>
+        <div className="container">{text}</div>
     )
 }
 
