390a06ecff0ff27c1f84452f9c5312362a72355b
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-04-23 20:54:44 +0600

    changed names

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -8,77 +8,23 @@ const News = (props) => {
         <div className='title'>daily news tips</div>
         <div className='sub-title'>the best places to see autumn colours</div>
       </div>
-      <div className='card__block'>
-        <div className='card'>
-          <div className='card__top-side'>
-            <div className='date'>
+      <div className='news-card-wrapper'>
+        <div className='news-card'>
+          <div className='news-card-header'>
+            <div className='news-card__date'>
               <span className='date__day'>15</span>
               <span className='date__month'>jun</span>
             </div>
-            <img className='image' src='https://img5.goodfon.com/wallpaper/big/0/de/devushka-ulybka-vzgliad-glaza-vesnushki-veronika-vonka-roma.jpg' alt='logo' />
+            <img className='news-card__image' src='https://img5.goodfon.com/wallpaper/big/0/de/devushka-ulybka-vzgliad-glaza-vesnushki-veronika-vonka-roma.jpg' alt='logo' />
           </div>
-          <div className='card-news__bottom-side'>
-            <div className='card-news__title'>15 Restaurants in London and New-York and Bishkek</div>
-            <div className='card-news__tag'>
+          <div className='news-card-footer'>
+            <div className='news-card__title'>15 Restaurants in London and New-York and Bishkek</div>
+            <div className='news-card__tag'>
               <span className='icon'>News</span>
               <span className='icon'>Comments</span>
             </div>
-            <p className='card-news__description'>Fusce suscipit nulla consequat, convallis odio in, ornare arcu. Nulla sit amet eleifend</p>
-            <button className='card-news__button'>read more</button>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card__top-side'>
-            <div className='date'>
-              <span className='date__day'>15</span>
-              <span className='date__month'>jun</span>
-            </div>
-            <img className='image' src='https://img5.goodfon.com/wallpaper/big/0/de/devushka-ulybka-vzgliad-glaza-vesnushki-veronika-vonka-roma.jpg' alt='logo' />
-          </div>
-          <div className='card-news__bottom-side'>
-            <div className='card-news__title'>15 Restaurants in London and New-York and Bishkek</div>
-            <div className='card-news__tag'>
-              <span className='icon'>News</span>
-              <span className='icon'>Comments</span>
-            </div>
-            <p className='card-news__description'>Fusce suscipit nulla consequat, convallis odio in, ornare arcu. Nulla sit amet eleifend</p>
-            <button className='card-news__button'>read more</button>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card__top-side'>
-            <div className='date'>
-              <span className='date__day'>15</span>
-              <span className='date__month'>jun</span>
-            </div>
-            <img className='image' src='https://img5.goodfon.com/wallpaper/big/0/de/devushka-ulybka-vzgliad-glaza-vesnushki-veronika-vonka-roma.jpg' alt='logo' />
-          </div>
-          <div className='card-news__bottom-side'>
-            <div className='card-news__title'>15 Restaurants in London and New-York and Bishkek</div>
-            <div className='card-news__tag'>
-              <span className='icon'>News</span>
-              <span className='icon'>Comments</span>
-            </div>
-            <p className='card-news__description'>Fusce suscipit nulla consequat, convallis odio in, ornare arcu. Nulla sit amet eleifend</p>
-            <button className='card-news__button'>read more</button>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card__top-side'>
-            <div className='date'>
-              <span className='date__day'>15</span>
-              <span className='date__month'>jun</span>
-            </div>
-            <img className='image' src='https://img5.goodfon.com/wallpaper/big/0/de/devushka-ulybka-vzgliad-glaza-vesnushki-veronika-vonka-roma.jpg' alt='logo' />
-          </div>
-          <div className='card-news__bottom-side'>
-            <div className='card-news__title'>15 Restaurants in London and New-York and Bishkek</div>
-            <div className='card-news__tag'>
-              <span className='icon'>News</span>
-              <span className='icon'>Comments</span>
-            </div>
-            <p className='card-news__description'>Fusce suscipit nulla consequat, convallis odio in, ornare arcu. Nulla sit amet eleifend</p>
-            <button className='card-news__button'>read more</button>
+            <p className='news-card__description'>Fusce suscipit nulla consequat, convallis odio in, ornare arcu. Nulla sit amet eleifend</p>
+            <button className='news-card-button'>read more</button>
           </div>
         </div>
       </div>
