3ee60c6c7379264e80431596b42d4b5c06606b8b
Bleakk <bleakk240699@gmail.com>
2019-04-23 20:24:29 +0600

    fix class name of Category component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -6,24 +6,24 @@ const Category = (props) => {
       <div class="category-container ">
         <div class="category container-Tynai">
           <div class="category-block first-block">
-            <img class="category__image"></img>
-            <span class="category__name">Hotel</span>
+            <img class="category-block__image"></img>
+            <span class="category-block__name">Hotel</span>
           </div>
           <div class="category-block">
-            <img class="category__image"></img>
-            <span class="category__name">Dinner</span>
+            <img class="category-block__image"></img>
+            <span class="category-block__name">Dinner</span>
           </div>
           <div class="category-block">
-            <img class="category__image"></img>
-            <span class="category__name">Destination</span>
+            <img class="category-block__image"></img>
+            <span class="category-block__name">Destination</span>
           </div>
           <div class="category-block">
-            <img class="category__image"></img>
-            <span class="category__name">Coffee</span>
+            <img class="category-block__image"></img>
+            <span class="category-block__name">Coffee</span>
           </div>
           <div class="category-block last-block">
-            <img class="category__image"></img>
-            <span class="category__name">Club and Bear</span>
+            <img class="category-block__image"></img>
+            <span class="category-block__name">Club and Bear</span>
           </div>
         </div>
       </div>
