8c9da6779eccf33dbabb8cb1967484e025e20517
E1emN <e1eman1337@gmail.com>
2019-04-25 19:56:16 +0600

    Upcomming events

--- src/components/UpcommingEvents/index.js
+++ src/components/UpcommingEvents/index.js
@@ -10,40 +10,24 @@ const UpcommingEvents = () => {
         consectetur adipiscing elit.
         Phasselus aliquam retrum 
       </span>
-      <div className="upcomming-events-events">
-        <div className="upcomming-events-events-event">
-          <div className="upcomming-events-events-event-title">
-            <span className="upcomming-events-events-event-title">Low Road Live</span>
-            <div className="upcomming-events-events-event-title-information">
-              <span className="upcomming-events-events-event-title-information__location">Festival, Night Live</span>
-              <span className="upcomming-events-events-event-title-information__price">120$</span>
-              <span className="upcomming-events-events-event-title-information__open"></span>
+      <div className="upcomming-events-wrapper">
+        <div className="upcomming-events-wrapper-card">
+          <div className="upcomming-events-wrapper-card-header">
+            <h5 className="upcomming-events-wrapper-card-header-name">Low Road Live</h5>
+            <div className="upcomming-events-wrapper-card-header-information">
+              <span className="upcomming-events-wrapper-card-header-information__location">Festival, Night Live</span>
+              <span className="upcomming-events-wrapper-card-header-information__price">120$</span>
+              <span className="upcomming-events-wrapper-card-header-information__open"></span>
             </div>
             
           </div>
-          <div className="upcomming-events-events-event-index">4,5</div>
-          <table className="upcomming-events-events-event-table">
+          <div className="upcomming-events-wrapper-card-rating">4,5</div>
+          <table className="upcomming-events-wrapper-card-table">
             <tr><th>Adress</th><th>From</th><th>To</th></tr>
             <tr><th>8-9 Hoxton Square London N1<br/> 6NU UK</th><th>July 22, 19<br/>10:15 AM</th><th>July 26, 19<br/>10:15 AM</th></tr>
           </table>
         </div>
 
-        <div className="upcomming-events-events-event">
-          <div className="upcomming-events-events-event-title">
-            <span className="upcomming-events-events-event-title">Low Road Live</span>
-            <div className="upcomming-events-events-event-title-information">
-              <span className="upcomming-events-events-event-title-information__location">Festival, Night Live</span>
-              <span className="upcomming-events-events-event-title-information__price">120$</span>
-              <span className="upcomming-events-events-event-title-information__open">Closed</span>
-            </div>
-            
-          </div>
-          <div className="upcomming-events-events-event-index">4,5</div>
-          <table className="upcomming-events-events-event-table">
-            <tr><th>Adress</th><th>From</th><th>To</th></tr>
-            <tr><th>8-9 Hoxton Square London N1<br/> 6NU UK</th><th>July 22, 19<br/>10:15 AM</th><th>July 26, 19<br/>10:15 AM</th></tr>
-          </table>
-        </div>
       </div>
     </div>
     </section>
