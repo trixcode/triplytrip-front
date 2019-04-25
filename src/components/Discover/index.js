b48264321ae308f3369e5d09d046c018ec63f924
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-04-25 19:16:01 +0600

    changed classes name

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -3,58 +3,60 @@ import './discover.scss';
 
 const Discover = (props) => {
   return (
-    <div className='container'>
-      <div className='title__block'>
-        <div className='title'>discover your dream places</div>
-        <div className='sub-title'>All the pathos and irony of leaving one’s youth behind is thus implicit in every joyous moment of travel: one knows that the first joy can never be recovered, and the wise traveler learns not to repeat successes but tries new places all the time.</div>
+    <section className='section-discover'>
+      <div className='container'>
+        <div className='section-top-wrapper'>
+          <div className='section-top__tittle'>discover your dream places</div>
+          <div className='section-top__sub-title'>All the pathos and irony of leaving one’s youth behind is thus implicit in every joyous moment of travel: one knows that the first joy can never be recovered, and the wise traveler learns not to repeat successes but tries new places all the time.</div>
+        </div>
+        <div className='discover-cards-wrapper'>
+          <div className='discover-card'>
+            <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
+            <div className='discover-card__info-text'>
+              <div className='discover-card__info-place'>
+                <span className='discover-card__info-place-name'>london</span>
+              </div>
+              <div className='discover-card__info-destination'>
+                <span className='discover-card__info-destination-name'>8 destination</span>
+              </div>
+            </div>
+          </div>
+          <div className='discover-card'>
+            <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
+            <div className='discover-card__info-text'>
+              <div className='discover-card__info-place'>
+                <span className='discover-card__info-place-name'>london</span>
+              </div>
+              <div className='discover-card__info-destination'>
+                <span className='discover-card__info-destination-name'>8 destination</span>
+              </div>
+            </div>
+          </div>
+          <div className='discover-card'>
+            <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
+            <div className='discover-card__info-text'>
+              <div className='discover-card__info-place'>
+                <span className='discover-card__info-place-name'>london</span>
+              </div>
+              <div className='discover-card__info-destination'>
+                <span className='discover-card__info-destination-name'>8 destination</span>
+              </div>
+            </div>
+          </div>
+          <div className='discover-card'>
+            <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
+            <div className='discover-card__info-text'>
+              <div className='discover-card__info-place'>
+                <span className='discover-card__info-place-name'>london</span>
+              </div>
+              <div className='discover-card__info-destination'>
+                <span className='discover-card__info-destination-name'>8 destination</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='discover-card-wrapper'>
-        <div className='discover-card'>
-          <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
-          <div className='discover-card-text-block'>
-            <div className='discover-card-place'>
-              <span className='discover-card-place__name'>london</span>
-            </div>
-            <div className='discover-card-destination'>
-              <span className='discover-card-destination__name'>8 destination</span>
-            </div>
-          </div>
-        </div>
-        <div className='discover-card'>
-          <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
-          <div className='discover-card-text-block'>
-            <div className='discover-card-place'>
-              <span className='discover-card-place__name'>london</span>
-            </div>
-            <div className='discover-card-destination'>
-              <span className='discover-card-destination__name'>8 destination</span>
-            </div>
-          </div>
-        </div>
-        <div className='discover-card'>
-          <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
-          <div className='discover-card-text-block'>
-            <div className='discover-card-place'>
-              <span className='discover-card-place__name'>london</span>
-            </div>
-            <div className='discover-card-destination'>
-              <span className='discover-card-destination__name'>8 destination</span>
-            </div>
-          </div>
-        </div>
-        <div className='discover-card'>
-          <img className='discover-card__image' src='https://static.wixstatic.com/media/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg/v1/fill/w_330,h_420,al_c,q_80,usm_0.66_1.00_0.01/c9dceb0320d64d4ab5995ff29a3e4a8e.jpg' alt='logo' />
-          <div className='discover-card-text-block'>
-            <div className='discover-card-place'>
-              <span className='discover-card-place__name'>london</span>
-            </div>
-            <div className='discover-card-destination'>
-              <span className='discover-card-destination__name'>8 destination</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </section>
   )
 }
 
