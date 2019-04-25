5ac77a45ec03b8a4f9aab92a7dedafd8115d43e6
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-04-25 18:21:28 +0600

    added discover component

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import Header from '../src/components/Header';
 
 import "../src/assets/styles/main.scss";
 import News from '../src/components/News';
+import Discover from '../src/components/Discover';
 
 function Home() {
     return <div className="example">
     <Header text="This is test"/>
+    <Discover />
     <News />
     </div>;
   }
