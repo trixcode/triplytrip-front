25eee8c1a603c39fb3ad164c03ac35e84a4e4660
Bleakk <bleakk240699@gmail.com>
2019-04-27 19:00:00 +0600

    add fontawesome

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,20 +1,32 @@
 
 import './search.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHotel  } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faMapMarkerAlt  } from '@fortawesome/free-solid-svg-icons';
 
 const Search = (props) => {
-    return (
-      <section className="search-container">
-        <div className="search container">
-          <form className="search-form">
+  return (
+    <section className="search-container">
+      <div className="search container">
+        <form className="search-form">
+          <div className="search-form__input">
             <input className="search-form__input-first" placeholder="Address, city or select suggestion category" />
-            <input className="search-form__input-second" placeholder="Location"></input>
-            <button className="search-form__button">search now</button>
-          </form>
-        </div>
-      </section>
-    )
+            {<FontAwesomeIcon
+              className='search-icon'
+              icon={faSearch}
+            />}
+          </div>
+          <div className="search-form__input">
+            <input className="search-form__input-second" placeholder="Location" />
+            {<FontAwesomeIcon
+            className='map'
+            icon={faMapMarkerAlt}
+          />}
+          </div>
+          <button className="search-form__button">search now</button>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default Search;
