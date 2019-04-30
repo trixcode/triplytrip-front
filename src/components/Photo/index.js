9d1af57b795ed69a3a8621c739d88357ba1f18c3
Bleakk <bleakk240699@gmail.com>
2019-04-30 18:39:18 +0600

    add photo component

---
+++ src/components/Photo/index.js
@@ -0,0 +1,12 @@
+
+import './photo.scss';
+
+const Photo = (props) => {
+  return (
+    <div className="article-banner">
+      <img src="../../../static/background-two.jpg" className="article-banner__photo" alt="background" />
+    </div>
+  )
+}
+
+export default Photo;
