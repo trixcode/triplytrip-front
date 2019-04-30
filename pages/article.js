e643e57916941f6b9c70a2e7bebd722cf7022ce4
Bleakk <bleakk240699@gmail.com>
2019-04-30 19:35:34 +0600

    fixed names of components

--- pages/article.js
+++ pages/article.js
@@ -3,6 +3,7 @@ import ArticleDescription from '../src/components/ArticleDescription';
 import ArticleSidebar from '../src/components/ArticleSidebar';
 import Header from '../src/components/Header';
 import DetailMainImage from '../src/components/DetailMainImage';
+import ArticleTitle from '../src/components/ArticleTitle';
 
 
 function Article() {
@@ -12,6 +13,7 @@ function Article() {
         <Header />
       </div>
       <DetailMainImage />
+      <ArticleTitle />
       <div className='article-wrapper'>
         <div className="container article-container">
           <ArticleDescription />
