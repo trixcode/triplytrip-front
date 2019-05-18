a1310257b2a12368903eca4f02c9be6b6cfd98bb
Bleakk <bleakk240699@gmail.com>
2019-05-18 18:03:12 +0600

    add media screen to top content and header

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -19,22 +19,24 @@ const Articles = (props) => {
         />
         <div className='news-cards-wrapper'>
           {
-            latestNewsList.map(latestNews => (
-              <div className='news-card' latestNews={latestNews}>
+            latestNewsList.map(latestnews => (
+              <div
+              key={latestnews.id} 
+              className='news-card' latestnews={latestnews}>
                 <div className='news-card-header'>
                   <div className='news-card__date'>
-                    <span className='date__day'>{moment(latestNews.createdDate).format('DD')}</span>
-                    <span className='date__month'>{moment(latestNews.createdDate).format('MMM')}</span>
+                    <span className='date__day'>{moment(latestnews.createdDate).format('DD')}</span>
+                    <span className='date__month'>{moment(latestnews.createdDate).format('MMM')}</span>
                   </div>
-                  <img className='news-card__image' src={latestNews.image} alt={latestNews.title} />
+                  <img className='news-card__image' src={latestnews.image} alt={latestnews.title} />
                 </div>
                 <div className='news-card-footer'>
-                  <div className='news-card__title'>{latestNews.title}</div>
+                  <div className='news-card__title'>{latestnews.title}</div>
                   <div className='news-card__tag'>
                     <span className='icon'>News</span>
                     <span className='icon'>Comments</span>
                   </div>
-                  <div dangerouslySetInnerHTML={{__html: latestNews.description}} className='news-card__description'></div>
+                  <div dangerouslySetInnerHTML={{__html: latestnews.description}} className='news-card__description'></div>
                   <button className='news-card-button'>read more</button>
                 </div>
               </div>
--- src/components/DetailTitle /index.js
+++ src/components/DetailTitle /index.js
@@ -1,34 +1,37 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
-import {  } from '@fortawesome/free-brands-svg-icons';
+import { } from '@fortawesome/free-brands-svg-icons';
 import './detailTitle.scss';
 
 const DetailTitle = (props) => {
   return (
     <section className="detail-title">
-      <div className="container detail-title__container">
-        <h2 className="detail-title__name">15 Restaurants in London and New York</h2>
-        <div className="detail-tags">
-          <div className="detail-tags__wrapper">
-            <div className="detail-tags__block">
-              <span className="detail-tags__title">Posted On</span>
-              <span className="detail-tags__subtitle">May 09, 2017</span>
+      <div className="detail-title__container container">
+        {/* <div className="detail-title__wrapper"> */}
+          <h2 className="detail-title__name">15 Restaurants in London and New York</h2>
+          <div className="detail-tags">
+            <div className="detail-tags__wrapper">
+              <div className="detail-tags__block">
+                <span className="detail-tags__title">Posted On</span>
+                <span className="detail-tags__subtitle">May 09, 2017</span>
+              </div>
+              <div className="detail-tags__block middle-block">
+                <span className="detail-tags__title">Category</span>
+                <span className="detail-tags__subtitle">Hotel</span>
+              </div>
+              <div className="detail-tags__block">
+                <span className="detail-tags__title">Comment</span>
+                <span className="detail-tags__subtitle detail-tags__digits">5</span>
+              </div>
             </div>
-            <div className="detail-tags__block middle-block">
-              <span className="detail-tags__title">Category</span>
-              <span className="detail-tags__subtitle">Hotel</span>
-            </div>
-            <div className="detail-tags__block">
-              <span className="detail-tags__title">Comment</span>
-              <span className="detail-tags__subtitle detail-tags__digits">5</span>
-            </div>
-          </div>
-          {<FontAwesomeIcon
-            className="detail-icon__share"
-            icon={faShareAlt}
-          />}
+            {<FontAwesomeIcon
+              className="detail-icon__share"
+              icon={faShareAlt}
+            />}
+          {/* </div> */}
         </div>
+
       </div>
     </section>
   )
