bf40ee4a39654b9b39995d562f1861b6bebdc786
AdiKu <adiletkub@gmail.com>
2019-05-22 10:40:49 +0600

    deleted trackline function and added radio buttons

--- src/components/FilterForm/index.js
+++ src/components/FilterForm/index.js
@@ -20,47 +20,10 @@ const FilterForm = props => {
     'coffe', 'ckin care', 'spa', 'parking street',
     'outdoor seating', 'wireless internet', 'park',
     'massage therapy', 'venues', 'jewellery', 'fashion']
-  
-  const setPointPosition = () => {
-    const distanceStaticLineLeftSide = document.getElementById('filter-form-radius-distance__static__line')
-    const point = document.getElementById('filter-form-radius-distance__point')
-    const selectLine = document.getElementById('filter-form-radius-distance__select__line')
-    if (event.x > distanceStaticLineLeftSide.getBoundingClientRect().left && event.x < distanceStaticLineLeftSide.getBoundingClientRect().right) {
-      point.style.left = event.x - distanceStaticLineLeftSide.getBoundingClientRect().left - 7 + 'px';
-      selectLine.style.width = event.x - distanceStaticLineLeftSide.getBoundingClientRect().left - 7 + 'px';
-    }
-    point.ondragstart = function () {
-      return false;
-    };
-  }
 
-  const setDistance = (unit) => {
-    const staticLine = document.getElementById('filter-form-radius-distance__static__line').getBoundingClientRect().width;
-    const pointLeft = 7 + parseInt(document.getElementById('filter-form-radius-distance__point').style.left)
-    const persent = Math.round(pointLeft * 100 / staticLine);
-    if (unit === 'km') {
-      return Math.round((100 - 1) / 100 * persent + 1)
-    }
-    else if (unit === 'm') {
-      return Math.round(((900 - 10) / 100 * persent + 10) / 10) * 10
-    }
-  }
-
-  useEffect(()=>{
-    Object.keys(document.querySelectorAll('.place-card__button')).map(key=>{
-      document.querySelectorAll('.place-card__button')[key].style.backgroundColor = '#e5f8fb';
-    });
-  })
   
   return (
     <div
-      onMouseUp={() => setpixels(false)}
-      onMouseMove={() => {
-        if (isMouseDown) {
-          setPointPosition()
-          setValue(setDistance(distanceUnit))
-        }
-      }}
       className='filter-form'>
       <div className='container'>
 
@@ -106,13 +69,12 @@ const FilterForm = props => {
             <span className='filter-form-radius__title'> Radius: </span>
             <p 
               className='filter-form-radius__dictance__number'> 
-              {distanceValue} 
+              {distanceUnit==='m'? distanceValue * 10 : distanceValue} 
               {distanceUnit} 
             </p>
             <select
               onChange={(event) => {
                 setUnit(event.target.value)
-                setValue(setDistance(event.target.value))
               }}
               className='filter-form__select radius__select'
               name="categorieslist"
@@ -123,19 +85,24 @@ const FilterForm = props => {
           </div>
 
           <div
-            onMouseDown={() => setpixels(true)}
             className='filter-form-radius-distance'>
             <div
-              style={{ left: '100px' }}
-              id='filter-form-radius-distance__point'
-              className='filter-form-radius-distance__point' />
-            <div
-              style={{ width: '100px' }}
+              style={{ width: distanceValue + '%' }}
               id='filter-form-radius-distance__select__line'
               className='filter-form-radius-distance__select__line' />
             <div
               id='filter-form-radius-distance__static__line'
               className='filter-form-radius-distance__static__line' />
+              <div className='filter-form-radius-distance__radio'>
+                {[1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100].map(key=>(
+                  <div    key={key}>
+                    <div
+                      id={key} 
+                      onClick={()=>setValue(event.target.id)}
+                      className='filter-form-radius-distance__radio__point'/>
+                  </div>
+                ))}
+              </div>
           </div>
 
         </div>
