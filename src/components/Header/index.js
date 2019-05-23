6463dc8f0066db13b4329330c7d80a261185f482
Bleakk <bleakk240699@gmail.com>
2019-05-23 18:17:34 +0600

    add mediaquery

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,78 @@
 import Link from 'next/link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './header.scss';
+
+const state = {
+  checkbox: false,
+  isOpen: false,
+  checked: false,
+}
+const HandleChangeColor = () => {
+  if(event.target.checked === true){
+    document.body.style = " ";
+  }else{
+    document.body.style = " "
+  }
+}
 
 
 const Header = () => {
+  
   return (
     <header className="header">
-      <div className="header-wrapper">
-        <Link href="/">
-          <a className="header-brand">
-            <img src='static/mainIcon.png' className="header-brand__logo" alt="logo" />
-            <h2 className="header-brand__title">TryplyTrip</h2>
-          </a>
-        </Link>
+      <div className="container">
+        <div className="header-wrapper">
+          <Link href="/">
+            <a className="header-brand">
+              <img src='static/mainIcon.png' className="header-brand__logo" alt="logo" />
+              <h2 className="header-brand__title">TryplyTrip</h2>
+            </a>
+          </Link>
 
-        <nav className="header-navigation">
-          <ul className="header-navigation-list">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Explore</a></li>
-            <li><a href="#">Listings</a></li>
-            <li><a href="#">Destinations</a></li>
-            <li><a href="#">Pages</a></li>
-            <li><a href="#">Blog</a></li>
-            <li>|</li>
-          </ul>
+          <nav className="header-navigation">
+            <ul className="header-navigation-list">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Explore</a></li>
+              <li><a href="#">Listings</a></li>
+              <li><a href="#">Destinations</a></li>
+              <li><a href="#">Pages</a></li>
+              <li><a href="#">Blog</a></li>
+              <li>|</li>
+            </ul>
 
-          <div className="header-actions">
-            <button>+ Add Listing</button>
-            <a href="#">icon</a>
-            <a href="#">icon</a>
+            <div className="header-actions">
+              <button>+ Add Listing</button>
+              <a href="#">icon</a>
+              <a href="#">icon</a>
+            </div>
+          </nav>
+
+          <div className="menu-toggle" id="menuToggle">
+
+            <input
+             type="checkbox"
+             value="menuOpen"
+            
+            //  checked="checked"
+            //  onClick={HandleChangeColor}
+             onChange={HandleChangeColor}
+              />
+            <span></span>
+            <span></span>
+            <span></span>
+            <ul id="menu" className="header-hamburger-list">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Explore</a></li>
+              <li><a href="#">Listings</a></li>
+              <li><a href="#">Destinations</a></li>
+              <li><a href="#">Pages</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
           </div>
-        </nav>
 
+        </div>
 
-        {/* <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label> */}
       </div>
+
 
     </header>
   )
