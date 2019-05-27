3b445be355eabbf460c8180acfbf1726de9dbacc
AdiKu <adiletkub@gmail.com>
2019-05-27 17:44:24 +0600

    madeing articleAside

--- src/components/ArticleAside/index.js
+++ src/components/ArticleAside/index.js
@@ -1,15 +1,102 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 import './articleAside.scss'
 
 
-const ArticleAside =(props)=> {
+const ArticleAside = (props) => {
   return (
-    <div className='articleAside'>
-      
-    </div>
+    <aside className='article-aside'>
+
+      <label className='article-aside__label'>
+        <input
+          className='article-aside__input'
+          placeholder='Search' />
+        <FontAwesomeIcon
+          className='article-aside__input__icon'
+          icon={faSearch} />
+      </label>
+
+      <div className='article-aside-box'>
+        <div className='article-aside-box__title'>
+          <div className='article-aside-box__title__trip' />
+          <h3 className='article-aside-box__title__text'>
+            Categories
+          </h3>
+        </div>
+
+        <ul className='zxz'>
+          <li className='article-aside-box__list'>
+            <a className='article-aside-box__link'>
+              coffee
+            </a>
+          </li>
+          <li className='article-aside-box__list'>
+            <a className='article-aside-box__link'>
+              news
+            </a>
+          </li>
+          <li className='article-aside-box__list'>
+            <a className='article-aside-box__link'>
+              tips & tricks
+            </a>
+          </li>
+          <li className='article-aside-box__list'>
+            <a className='article-aside-box__link'>
+              uncategorized
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className='article-aside-box'>
+
+        <div className='article-aside-box__title'>
+          <div className='article-aside-box__title__trip' />
+          <h3 className='article-aside-box__title__text'>
+            Latest News
+            </h3>
+        </div>
+        <div className='article-aside-box__news'>
+          <div className='article-aside-box__news__img__wrapper'>
+            <img
+              src='https://img3.goodfon.com/original/1440x900/2/29/cafe-terrace-old-fashioned.jpg'
+              className='article-aside-box__news__img' />
+          </div>
+          <div className='article-aside-box__news__description'>
+            <h4 className='article-aside-box__news__description__text'>
+              15 Restaurants in Bishkek and Almata
+            </h4>
+            <span className='article-aside-box__news__description__date'>
+              April 11, 2018
+            </span>
+          </div>
+        </div>
+
+
+      </div>
+      <div className='article-aside-box'>
+        <div className='article-aside-box__title'>
+          <div className='article-aside-box__title__trip' />
+          <h3 className='article-aside-box__title__text'>
+            Newsletter
+              </h3>
+        </div>
+        <span className='article-aside-box__newsletter__text'>
+          
+        </span>
+        <label className='article-aside-box__newsletter__label'>
+          <input className='article-aside-box__newsletter__input'/>
+          <FontAwesomeIcon
+          className='article-aside__newsletter__input__icon'
+          icon={faPaperPlane} />
+        </label>
+      </div>
+      <div className='article-aside-box__ad__wrapper'>
+    
+      </div>
+
+    </aside>
   )
 }
 
