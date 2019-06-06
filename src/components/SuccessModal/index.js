76cd69e3468ed8e283e12241afb285e72d16420b
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-06-06 19:24:19 +0600

    added succes modal style

--- src/components/SuccessModal/index.js
+++ src/components/SuccessModal/index.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import { setDefaultResponseStatuses } from '../../store/actions';
 import './successModal.scss';
 
 
-const SuccessModal = ({responseStatuses, setDefaultResponseStatusesAction}) => {
+const SuccessModal = ({ responseStatuses, setDefaultResponseStatusesAction }) => {
   return (
     <Modal
       className="modal-container"
@@ -17,11 +18,31 @@ const SuccessModal = ({responseStatuses, setDefaultResponseStatusesAction}) => {
       onRequestClose={setDefaultResponseStatusesAction}
       overlayClassName="Overlay"
     >
+      <div className='modal-content'>
       <button
-        className="modal-close-button"
+        className="modal-close"
         onClick={setDefaultResponseStatusesAction}>
-        Close Modal
+        <FontAwesomeIcon
+          className='modal-close__icon'
+          icon={faTimes}
+        />
       </button>
+        <h3 className='modal-content__title'>success!</h3>
+        <p className='modal-content__description'>
+          Your account has been created.
+        </p>
+        <div>
+          <FontAwesomeIcon
+            className='modal-content__icon'
+            icon={faCheckCircle}
+          />
+        </div>
+        <button
+          className="modal-content-button"
+          onClick={setDefaultResponseStatusesAction}>
+          continue
+      </button>
+      </div>
     </Modal>
   )
 }
