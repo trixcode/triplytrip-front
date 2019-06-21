0a726afb12bf548b3f6595c5083063457ab9a866
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2019-06-21 14:45:58 +0600

    fixed warnings

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Field } from 'redux-form'
 import { customInputField } from '../CustomFields';
 
--- src/components/SubscribeForm/index.js
+++ src/components/SubscribeForm/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Field } from 'redux-form'
 import { customInputField } from '../CustomFields';
 
